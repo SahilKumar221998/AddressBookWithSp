e22fe67cca38d4ee27e6bb714d8e97075913cfd9
SahilKumar221998 <sahilja265@gmail.com>
2024-07-06 12:25:21 +0530

    User Defined Exception for UserNames

---
+++ UserNameExistException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookByUsingAdo.Net
+{
+    public class UserNameExistException:Exception
+    {
+        public UserNameExistException(string Message):base(Message) { }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
