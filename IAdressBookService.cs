d5161a3f4c4005beea27ff8c95c038a7ad946a38
SahilKumar221998 <sahilja265@gmail.com>
2024-07-06 12:21:23 +0530

    Interface with all abstract crud methods

---
+++ IAdressBookService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookByUsingAdo.Net
+{
+    public interface IAdressBookService
+    {
+        void choices();
+        void createDataBase(string dataBaseName);
+        void contactPersonData();
+        void insertContactPerson(ContactPerson person);
+        void updateContactPerson(string firstName);
+        void deleteContactPerson(string firstName);
+        void getAllContactPerson();
+        void getAllContactPersonBasedOnCity(string city);
+        void getAllContactPersonBasedOnState( string state);
+    }
+}
