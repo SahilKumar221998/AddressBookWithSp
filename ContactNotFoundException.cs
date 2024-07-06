b5093af42c5dd532bf2082c7ca5f7b2396020d8f
SahilKumar221998 <sahilja265@gmail.com>
2024-07-06 12:22:29 +0530

    User Defined Exception if contact not found

---
+++ ContactNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookByUsingAdo.Net
+{
+    public class ContactNotFoundException:Exception
+    {
+        public ContactNotFoundException(string Message):base(Message)
+        {
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
