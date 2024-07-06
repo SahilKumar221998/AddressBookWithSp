cbaa64518fc30c005396a94a954a374f82f9795a
SahilKumar221998 <sahilja265@gmail.com>
2024-07-06 12:18:56 +0530

    Address Book with Main method

---
+++ AddressBookMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookByUsingAdo.Net
+{
+    public class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            IAdressBookService addressBookService = new AddressBookServiceImpl();
+            addressBookService.choices();
+        }
+    }
+}
